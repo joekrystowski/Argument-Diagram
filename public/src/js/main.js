517f3a36473ac260ed39c4262d4af5b467176b83
Svet <70716489+ikyman@users.noreply.github.com>
2021-05-29 23:09:48 -0400

    Part 1 of click-toggle

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -18,3 +18,18 @@ let arg2 = createArgument();
 //testing
 let test = createDependentPremise(arg1.rect, arg2.rect);
 
+// used to alternate between selected and unselected when clicking on the elment.
+paper.on("element:pointerclick", function(eventView){
+  if (eventView._toolsView.tools.length <= 0){
+    console.log("What is this? an element with no tools?")
+    console.log("Well that is quite strange.");
+    return
+  }
+  if(eventView._toolsView.tools[0].isVisible() == true){
+    //console.log("UnClicked=>Hiding!");
+    eventView.hideTools();
+  }else{
+    //console.log("Clicked=>Showing!");
+    eventView.showTools();
+  }
+});
