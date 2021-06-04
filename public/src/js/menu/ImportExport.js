bc7004efcd53153ea6f27a20b9c42c4353cab42f
Cheng Bin Chen <chengbinchen1@gmail.com>
2021-06-04 17:17:56 -0400

    js file

---
+++ public/src/js/menu/ImportExport.js
@@ -0,0 +1,25 @@
+import { graph } from '../graph.js';
+import util from 'util';
+export function exportGraph() {
+    const data = util.inspect(graph.toJSON());
+    const filename = "myDiagram.JSON";
+    const file = new Blob([data], { type: "application/json" });
+    if (window.navigator.msSaveOrOpenBlob) { // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    }
+    else { // Others
+        const a = document.createElement("a");
+        const url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function () {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+        }, 0);
+    }
+}
+export function importGraph() {
+    console.log('hello');
+}
