31e2579d3fc382fae79ebcd85273383dbe4d0680
Eddie Krystowski <eddiekrystowski@gmail.com>
2021-06-05 19:45:02 -0400

    fixed bugs

--- public/src/ts/menu/CreateArguments.ts
+++ public/src/ts/menu/CreateArguments.ts
@@ -6,19 +6,20 @@ import { Argument } from '../Argument.js'
 import { DependentPremise } from '../DependentPremise.js';
 
 //when new-argument-button is clicked
-export function createArgument(x:number, y:number, props:any={}) {
+export function createArgument(x:number, y:number) {
   //creating new rect (Joint.js object)
   const new_rect = new Argument({
-    x: props.x || x,
-    y: props.y || y,
-    text: (props.attrs ? props.text.text : (props.text || "Argument") ),
-    type: props.type || "argument",
+    x: x,
+    y: y,
+    text: "Argument",
+    type: "argument",
     body_color: color.argument.bodyColor,
     text_color: color.argument.textColor, 
     stroke: color.argument.stroke,
     link_color: color.argument.linkColor,
     weight: "1.0"
   });
+
   //add new rect to the graph for displaying
   new_rect.rect.addTo(graph);
   //adds the buttons to each rect
@@ -27,19 +28,20 @@ export function createArgument(x:number, y:number, props:any={}) {
 }
 
 //when objection-button is clicked
-export function createObjection(x:number, y:number, props:any={}) {
+export function createObjection(x:number, y:number) {
   //creating new rect (Joint.js object)
   const new_rect = new Argument({
-    x: props.x || x,
-    y: props.y || y,
-    text: props.attrs.text.text || props.text || "Objection",
-    type: props.type || "objection",
+    x: x,
+    y: y,
+    text: "Objection",
+    type: "objection",
     body_color: color.objection.bodyColor,
     text_color: color.objection.textColor,
     stroke: color.objection.stroke, 
     link_color: color.objection.linkColor,
     weight: "-1.0"
   });
+  
   new_rect.rect.addTo(graph);
   //adds the buttons to each rect
   addRectTools(new_rect.rect);
@@ -68,8 +70,6 @@ export function createDependentPremise(rect1: joint.shapes.app.CustomRect, rect2
   new_dependent_premise.rect.addTo(graph);
   //adds the buttons to each rect
   addDependentPremiseTools(new_dependent_premise.rect);
-
-  console.log(new_dependent_premise);
 
   return new_dependent_premise;
 }
