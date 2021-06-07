0c66bea10e6f5f6a5d72f9fad122b27072b8ec1c
Brian Wu <briantehwu@gmail.com>
2021-06-07 02:47:02 -0400

    Added new UI
    Added new toolbar
    Changed colors
    Fixed edit container width

--- public/src/js/Argument.js
+++ public/src/js/Argument.js
@@ -1,17 +1,34 @@
 import { calcHeight } from "./util.js";
 /* global joint */
 const joint = window.joint;
+// const CustomRect = joint.shapes.standard.Rectangle.define("app.CustomRect", {
+//   markup: '<g class="rotatable"><g class="scalable"><rect/></g><text/></g>',
+//   attrs: {
+//     rect: { class:"argument-rect", width: 100, height: 100}, //, fill: "white", stroke: "black", width: 100, height: 100 },
+//     text: { class:"argument-text" ,
+//       "font-size": 12,
+//       "ref-x": 0.5,
+//       "ref-y": 0.5,
+//       ref: "rect",
+//       "y-alignment": "middle",
+//       "x-alignment": "middle",
+//     },
+//   },
+//   link_color: "black",
+//   weight: "1",
+//   type: "none",
+// }); 
 const CustomRect = joint.shapes.standard.Rectangle.define("app.CustomRect", {
-    markup: '<g class="rotatable"><g class="scalable"><rect/></g><text/></g>',
+    // markup: '<g class="rotatable"><g class="scalable"><rect/></g><text/></g>',
     attrs: {
-        rect: { fill: "white", stroke: "black", width: 100, height: 100 },
-        text: {
-            "font-size": 12,
-            "ref-x": 0.5,
-            "ref-y": 0.5,
-            ref: "rect",
-            "y-alignment": "middle",
-            "x-alignment": "middle",
+        body: {
+            class: "custom-rect",
+            rx: 10,
+            ry: 10,
+            fill: '#ffffff'
+        },
+        label: {
+            fontSize: 12
         },
     },
     link_color: "black",
@@ -75,6 +92,7 @@ export class Argument {
             },
             attrs: {
                 rect: {
+                    // class: "arg",
                     fill: config.body_color,
                 },
                 text: {
--- public/src/ts/main.ts
+++ public/src/ts/main.ts
@@ -1,10 +1,14 @@
 /* global joint createDependentPremise */
 // const joint = window.joint;
-import { saveEdits } from './menu/SaveEditsButton.js';
-import { createArgument, createObjection, createDependentPremise } from './menu/CreateArguments.js';
-import { Argument } from './Argument.js';
-import { color } from './colors.js';
-import { paper, graph } from './graph.js';
+import { saveEdits } from "./menu/SaveEditsButton.js";
+import {
+  createArgument,
+  createObjection,
+  createDependentPremise,
+} from "./menu/CreateArguments.js";
+import { Argument } from "./Argument.js";
+import { color } from "./colors.js";
+import { paper, graph } from "./graph.js";
 
 // this is built on Joint.js, an open source library. It handles a lot of the
 // fundamental pieces for us on the back end, we have to implement the front end / interface
@@ -20,89 +24,98 @@ import { paper, graph } from './graph.js';
 // });
 
 // console.log("setup");
-const newArgumentButton = document.getElementById("new-argument-button") as HTMLElement;
-newArgumentButton.addEventListener("click", createArgument.bind(null, 100, 100));
+const argumentImage = new Image();
+argumentImage.src = "src/img/Argument.jpg";
+
+const newArgumentButton = document.getElementById(
+  "new-argument-button"
+) as HTMLElement;
+newArgumentButton.addEventListener(
+  "click",
+  createArgument.bind(null, 100, 100)
+);
 newArgumentButton.addEventListener("dragstart", (event) => {
-    event.dataTransfer?.setData('type', 'argument');
-})
+  // event.dataTransfer?.setDragImage(argumentImage, argumentImage.naturalWidth/2, argumentImage.naturalHeight/2);
+  event.dataTransfer?.setDragImage(argumentImage, 0, 0);
+  event.dataTransfer?.setData("type", "argument");
+});
 
-const objectionButton = document.getElementById("new-objection-button") as HTMLElement;
-objectionButton.addEventListener("click", createObjection.bind(null, 100, 100));
-objectionButton.addEventListener("dragstart", (event) => {
-    event.dataTransfer?.setData('type', 'objection');
-})
+// const objectionButton = document.getElementById("new-objection-button") as HTMLElement;
+// objectionButton.addEventListener("click", createObjection.bind(null, 100, 100));
+// objectionButton.addEventListener("dragstart", (event) => {
+//     event.dataTransfer?.setData('type', 'objection');
+// })
 
-const saveEditButton = document.getElementById("save-edit-button") as HTMLElement;
-saveEditButton.addEventListener("click",saveEdits);
+const saveEditButton = document.getElementById(
+  "save-edit-button"
+) as HTMLElement;
+saveEditButton.addEventListener("click", saveEdits);
 
 const paperContainer = document.getElementById("myholder") as HTMLElement;
 paperContainer.addEventListener("dragover", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 });
 paperContainer.addEventListener("drop", (event) => {
-    const type = event.dataTransfer?.getData('type');
-    if(type === 'argument') {
-        createArgument(
-            event.clientX - paperContainer.getBoundingClientRect().left,
-            event.clientY - paperContainer.getBoundingClientRect().top
-        );
-    }
-    else if (type === 'objection') {
-        createObjection(
-            event.clientX - paperContainer.getBoundingClientRect().left,
-            event.clientY - paperContainer.getBoundingClientRect().top
-        );
-    }
-    else {
-        throw new Error("Something went wrong when determining dataTransfer type.");
-    }
-})
+  const type = event.dataTransfer?.getData("type");
+  if (type === "argument") {
+    createArgument(
+      event.clientX - paperContainer.getBoundingClientRect().left,
+      event.clientY - paperContainer.getBoundingClientRect().top
+    );
+  } else if (type === "objection") {
+    createObjection(
+      event.clientX - paperContainer.getBoundingClientRect().left,
+      event.clientY - paperContainer.getBoundingClientRect().top
+    );
+  } else {
+    throw new Error("Something went wrong when determining dataTransfer type.");
+  }
+});
 
-const editContainer= $('#edit-container');
-editContainer.hide();
+// const editContainer= $('#edit-container');
+// editContainer.hide();
 
-function toggleHeader() {
-    const button = $('#toggleHeaderButton');
-    const header = $('#header');
-    button.toggleClass('collapsed');
-    console.log(button);
-    console.log(button.find('i'));
-    if(button.hasClass('collapsed')){
-        header.css('height', '0');
-        header.find('.wrapper').hide();
-        button.find('i').removeClass('fa-chevron-up');
-        button.find('i').addClass('fa-chevron-down');
-    }
-    else {
-        header.css('height', '100px');
-        header.find('.wrapper').show();
-        button.find('i').removeClass('fa-chevron-down');
-        button.find('i').addClass('fa-chevron-up');
-    }
-}
+// function toggleHeader() {
+//     const button = $('#toggleHeaderButton');
+//     const header = $('#header');
+//     button.toggleClass('collapsed');
+//     console.log(button);
+//     console.log(button.find('i'));
+//     if(button.hasClass('collapsed')){
+//         header.css('height', '0');
+//         header.find('.wrapper').hide();
+//         button.find('i').removeClass('fa-chevron-up');
+//         button.find('i').addClass('fa-chevron-down');
+//     }
+//     else {
+//         header.css('height', '100px');
+//         header.find('.wrapper').show();
+//         button.find('i').removeClass('fa-chevron-down');
+//         button.find('i').addClass('fa-chevron-up');
+//     }
+// }
 
-function hoverDropdown(element:HTMLElement) {
-    const content = $(element).find('.dropdown-content');
-    content.toggleClass('collapsed');
-    if(content.hasClass('collapsed')){
-        $(element).find('i.chevron').removeClass('fa-chevron-down');
-        $(element).find('i.chevron').addClass('fa-chevron-right');
-    }
-    else {
-        $(element).find('i.chevron').removeClass('fa-chevron-right');
-        $(element).find('i.chevron').addClass('fa-chevron-down');
-    }
-}
+// function hoverDropdown(element:HTMLElement) {
+//     const content = $(element).find('.dropdown-content');
+//     content.toggleClass('collapsed');
+//     if(content.hasClass('collapsed')){
+//         $(element).find('i.chevron').removeClass('fa-chevron-down');
+//         $(element).find('i.chevron').addClass('fa-chevron-right');
+//     }
+//     else {
+//         $(element).find('i.chevron').removeClass('fa-chevron-right');
+//         $(element).find('i.chevron').addClass('fa-chevron-down');
+//     }
+// }
 
-const addDropdown = document.getElementById('addDropdown') as HTMLElement;
-addDropdown.addEventListener('mouseenter', hoverDropdown.bind(null, addDropdown));
-addDropdown.addEventListener('mouseleave', hoverDropdown.bind(null, addDropdown));
+// const addDropdown = document.getElementById('addDropdown') as HTMLElement;
+// addDropdown.addEventListener('mouseenter', hoverDropdown.bind(null, addDropdown));
+// addDropdown.addEventListener('mouseleave', hoverDropdown.bind(null, addDropdown));
 
+// const toggleHeaderButton = document.getElementById('toggleHeaderButton') as HTMLElement;
+// toggleHeaderButton.addEventListener('click', toggleHeader);
 
-const toggleHeaderButton = document.getElementById('toggleHeaderButton') as HTMLElement;
-toggleHeaderButton.addEventListener('click', toggleHeader);
-
-
+console.log("creatingArguments");
 let arg1 = createArgument(100, 100);
 let arg2 = createArgument(300, 100);
 //testing
