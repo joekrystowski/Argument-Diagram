e61665f3ba33a6911a674af067641c71d670b2fe
Eddie Krystowski <eddiekrystowski@gmail.com>
2021-06-21 19:42:47 -0400

    checkpoint

--- public/src/ts/menu/CreateClaim.ts
+++ public/src/ts/menu/CreateClaim.ts
@@ -22,7 +22,6 @@ export function createClaim(x:number, y:number, text?:string) {
     weight: "1.0"
   });
 
-  console.log('legend from outside', legend);
   legend.add(new_rect);
 
   //add new rect to the graph for displaying
