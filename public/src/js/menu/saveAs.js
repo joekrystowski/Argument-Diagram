3766a435d4a696a6885b5adb7397c8ea54735730
Cheng Bin Chen <chengbinchen1@gmail.com>
2021-06-26 05:20:58 -0400

    new js file

---
+++ public/src/js/menu/saveAs.js
@@ -0,0 +1,26 @@
+import { paper } from '../graph.js';
+import { save } from '../util.js';
+export function savePNG() {
+    const filename = "myDiagram.png";
+    const svg = paper.svg;
+    const svgAsXML = (new XMLSerializer).serializeToString(svg);
+    const dataURL = "data:image/svg+xml," + encodeURIComponent(svgAsXML);
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+    img.onload = function (e) {
+        ctx.drawImage(img, 0, 0);
+    };
+    img.src = dataURL;
+    const data = canvas.toDataURL("image/png");
+    save(data, "image/png", filename);
+    /*
+    // save as svg only - need to get styling to work
+    var dl = document.createElement("a");
+    document.body.appendChild(dl); // This line makes it work in Firefox.
+    dl.setAttribute("href", dataURL);
+    dl.setAttribute("download", "test.svg");
+    dl.click(); */
+}
+export function savePDF() {
+}
