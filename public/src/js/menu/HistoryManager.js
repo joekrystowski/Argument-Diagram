74fde5ac0724e0c94e0fd808b271ae911f7af21f
Cheng Bin Chen <chengbinchen1@gmail.com>
2021-07-07 00:07:29 -0400

    add action

--- public/src/js/menu/HistoryManager.js
+++ public/src/js/menu/HistoryManager.js
@@ -2,7 +2,10 @@ export class Action {
     constructor(action) {
         this.type = action;
     }
-    execute() {
+    doAction() {
+        //if (this.type === "add-claim") createClaim();
+    }
+    undoAction() {
     }
 }
 export class History {
@@ -10,9 +13,24 @@ export class History {
         this.undoStack = [];
         this.redoStack = [];
     }
+    add(action) {
+        this.redoStack = [];
+        this.undoStack.push(action);
+    }
     undo() {
+        if (this.undoStack.length > 0) {
+            // pop next action and undo it
+            const action = this.undoStack.pop();
+            action.undoAction();
+            this.redoStack.push(action);
+        }
     }
     redo() {
+        if (this.redoStack.length > 0) {
+            // pop next action and redo it
+            const action = this.redoStack.pop();
+            action.doAction();
+            this.undoStack.push(action);
+        }
     }
 }
-export const historyManager = new History();
--- public/src/ts/menu/HistoryManager.ts
+++ public/src/ts/menu/HistoryManager.ts
@@ -1,3 +1,5 @@
+import { createClaim } from "./CreateClaim";
+
 export class Action {
   private type: String;
 
@@ -5,27 +7,47 @@ export class Action {
     this.type = action;
   }
 
-  public execute() {
-    
+  public doAction() {
+    //if (this.type === "add-claim") createClaim();
   }
+
+  public undoAction() {
+
+  }
+
+
 }
 
 export class History {
-  private undoStack: [];
-  private redoStack: [];
+  private undoStack: Action[];
+  private redoStack: Action[];
 
   public constructor() {
     this.undoStack = [];
     this.redoStack = [];
   }
 
-  public undo() {
-
+  public add(action: Action): void {
+    this.redoStack = [];
+    this.undoStack.push(action);
   }
 
-  public redo() {
-
+  public undo(): void {
+    if (this.undoStack.length > 0) {
+      // pop next action and undo it
+      const action = this.undoStack.pop()!;
+      action.undoAction();
+      this.redoStack.push(action);
+    }
   }
+
+  public redo(): void {
+    if (this.redoStack.length > 0) {
+      // pop next action and redo it
+      const action = this.redoStack.pop()!;
+      action.doAction();
+      this.undoStack.push(action);
+    } 
+  }
+
 }
-
-export const historyManager = new History();
