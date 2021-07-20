7fc22a042055adff8aa0e42bff50066d502587c1
joekrystowski <65693478+joekrystowski@users.noreply.github.com>
2021-07-20 10:06:23 -0400

    Automatic cleanup (#75)
    * link summation, objection fixes, markup fixes, color gradient, validity and link weight editing
    
    * a lot of link summation and objection changes
    
    * display summation in alert
    
    * automatic clean up button
    
    Co-authored-by: Joe Krystowski <joekrystowski@Joes-MacBook-Pro.local>

--- public/src/ts/main.ts
+++ public/src/ts/main.ts
@@ -13,6 +13,7 @@ import { Claim } from "./Claim.js";
 import { color } from "./colors.js";
 import { paper, graph } from "./graph.js";
 import { evaluateArgument } from "./menu/EvaluateArgument.js"
+import { AutomaticCleanUp } from "./menu/CleanUp/AutomaticCleanUp.js"
 import { createLink } from "./tools/LinkButton.js";
 
 const claimImage = new Image();
@@ -65,14 +66,17 @@ importButton.addEventListener("click", importGraph);
 const exportButton = document.getElementById("export-button") as HTMLElement;
 exportButton.addEventListener("click", exportGraph);
 
+const evaluateButton = document.getElementById('evaluate-button') as HTMLElement;
+evaluateButton.addEventListener('click', evaluateArgument);
+
+const CleanArgumentButton = document.getElementById('clean-argument-button') as HTMLElement;
+CleanArgumentButton.addEventListener('click', AutomaticCleanUp)
+
 const PNGButton = document.getElementById("png-button") as HTMLElement;
 PNGButton.addEventListener("click", savePNG);
 
 const PDFButton = document.getElementById("pdf-button") as HTMLElement;
 PDFButton.addEventListener("click", savePDF);
-
-const evaluateButton = document.getElementById('evaluate-button') as HTMLElement;
-evaluateButton.addEventListener('click', evaluateArgument);
 
 const sidePanel = document.getElementById("side-panel") as HTMLElement;
 const wrapper = document.getElementById("wrapper") as HTMLElement;
